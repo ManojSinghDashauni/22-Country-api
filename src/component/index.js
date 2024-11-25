73a0e2399bc1de7a7ac671aa4ec4ad3d1fe790e5
ManojSinghDashauni <dashauni007@gmail.com>
2024-11-25 15:36:00 +0530

    final

--- src/component/index.js
+++ src/component/index.js
@@ -1,8 +1,7 @@
 import Button from "./Button";
-import Country from "./country";
 import Mode from "./Mode";
 import Navlink from "./Navlink";
 import Home from "./Home";
 import Card from "./Card";
 
-export { Button, Country, Mode, Navlink, Home, Card };
+export { Button, Mode, Navlink, Home, Card };
